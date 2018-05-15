16ac8af0462f778bd779867b3d316a708fd2a793
misskone690@gmail.com <salimatakoneTelco@gmail.com>
2018-05-15 13:10:31 +0200

    Commit Zero

--- resources/views/description.blade.php
+++ resources/views/description.blade.php
@@ -78,9 +78,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
 						<ul class="nav navbar-nav navbar-right">
-							<li><a href="index" class="active">Accueil</a></li>
+							<li><a href="index">Accueil</a></li>
 							<li><a href="profil">Profil</a></li>
-							<li><a href="description">Description</a></li>
+							<li><a href="description" class="active">Description</a></li>
 							<li><a href="contact">Contact</a></li>
 						</ul>
 
