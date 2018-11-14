178d8ebc3ae130ac7fa365a69230efe2fbb52d99
salimatadev <salimatakoneTelco@gmail.com>
2018-11-14 09:45:00 +0100

    modification texte

--- resources/views/description.blade.php
+++ resources/views/description.blade.php
@@ -52,11 +52,11 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 							<h2>1. Contexte du jeu</h2>
 							<dd>
 							<p>« Le bon sens est la chose du monde la mieux partagée », dit René Descartes. C'est ainsi que le désir de célébrer et d'être célébré, de contenter et d'être contenté reste une chose que nous partageons tous, parents, amis, collègues, clients, « followers » sur les réseaux sociaux …<br>
-							En effet, il nous tient tous à cœur de souhaiter un vibrant <strong>Joyeux Anniversaire</strong> avec un somptueux présent à nos Amis [parents, amis, collègues, clients, partenaires, followers…] le jour de la célébration de leur naissance. Aussi, tout un chacun rêve d'avoir un <strong>Anniversaire Inoubliable,</strong> un <strong>Incroyable Anniversaire </strong>où nous sommes arrosés de divers cadeaux. Cependant, les contraintes de notre quotidien, difficultés financières, indisponibilité due aux voyages, au travail font que très souvent nous n'arrivons pas à réaliser notre rêve ou notre souhait. Notre Platforme de Jeu <strong>"Mon Incroyable Anniversaire</strong> va donner l'opportunité à tous ces Wishers and Dreamers de réaliser leur rêve.</p>
+							En effet, il nous tient tous à cœur de souhaiter un vibrant <strong>Joyeux Anniversaire</strong> avec un somptueux présent à nos Amis [parents, amis, collègues, clients, partenaires, followers…] le jour de la célébration de leur naissance. Aussi, tout un chacun rêve d'avoir un <strong>Anniversaire Inoubliable,</strong> un <strong>Incroyable Anniversaire </strong>où nous sommes arrosés de divers cadeaux. Cependant, les contraintes de notre quotidien, difficultés financières, indisponibilité due aux voyages, au travail font que très souvent nous n'arrivons pas à réaliser notre rêve ou notre souhait. Notre Platforme de compétition <strong>"Mon Incroyable Anniversaire</strong> va donner l'opportunité à tous ces Wishers and Dreamers de réaliser leur rêve.</p>
 							</dd>
 						</dt>
 						<dt>
-							<h2>2. Comment participer au jeu</h2>
+							<h2>2. Comment participer au concours</h2>
 							<dd>
 							<p>Inscrivez vous à partir de votre numéro ORANGE ou MTN. Envoyez par sms vos informations de la manière suivante I,Nom,Prénom,Jour-Mois au 459 abonné MTN et au 98164 abonné Orange. Exemple : I,konan,samuel,12-07  NB : Pas d’espace ni d’accent dans la syntaxe d’inscription.
 							Le coût de l'inscription est de 103fcfa.</p>
@@ -64,12 +64,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 						</dt>
 						<dt>
 							<dd>
-							<h2>3. Fonctionnement du jeu</h2>
+							<h2>3. Fonctionnement du concours</h2>
 							</dd>
 							<dt>
 								<dd>
 								<strong><span>3.1 Informer vos amis</span></strong>
-								<p>Le jour de votre anniversaire, un message vous sera envoyé pour vous rappeler votre anniversaire et comment vous pourrez voter pour vous-même. Egalement à vos amis, seront envoyés des messages leur rappelant votre participation au jeu et comment voter pour vous afin de vous faire gagner un super cadeau d'anniversaire.</p>
+								<p>Le jour de votre anniversaire, un message vous sera envoyé pour vous rappeler votre anniversaire et comment vous pourrez voter pour vous-même. Egalement à vos amis, seront envoyés des messages leur rappelant votre participation à la compétition et comment voter pour vous afin de vous faire gagner un super cadeau d'anniversaire.</p>
 								</dd>
 								<dd>
 								<strong><span>3.2 Comment voter pour un ami en compétition ?</span></strong>
@@ -79,14 +79,14 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 								</dd>
 								<dd>
 								<strong><span>3.3 Comment suivre le concours ?</span></strong>
-								<p>Vous pouvez à tout moment suivre votre classement et la tendance sur notre site internet (www.telcoanniv.com/ci). Vous pourrez voir de façon dynamique tous les candidats concourant ce jour ainsi que leur position. Et plus précisément, vous aurez un zoom sur les dix en tête de liste ainsi que le lot du jour.</p>
+								<p>Vous pouvez à tout moment suivre votre classement et la tendance sur notre site internet (www.telcoanniv.com). Vous pourrez voir de façon dynamique tous les candidats concourant ce jour ainsi que leur position. Et plus précisément, vous aurez un zoom sur les dix en tête de liste ainsi que le lot du jour.</p>
 								</dd>
 							</dt>
 						</dt>
 						<dt>
 							<h2>4. Sélection du gagnant</h2>
 							<dd>
-							<p>À la fin de la journée (23H), le candidat qui aura eu le nombre maximal de vote (SMS) se verra vainqueur du jeu pendant cette journée et bénéficiera d'un super <strong>cadeau d'anniversaire.</strong> La photo du vainqueur ainsi que son lot seront affichés sur notre site web (www.telcoanniv.com/ci).</p>
+							<p>À la fin de la journée (23H), le candidat qui aura eu le nombre maximal de vote (SMS) se verra vainqueur de la compétition pendant cette journée et bénéficiera d'un super <strong>cadeau d'anniversaire.</strong> La photo du vainqueur ainsi que son lot seront affichés sur notre site web (www.telcoanniv.com).</p>
 							</dd>
 						</dt>
 						<dt>
